aee7095d059a26479433a36166f770e99bb8f268
kvarcas91 <e.slutas@gmail.com>
2020-01-15 15:58:38 +0000

    claimList

--- platforms/android/app/src/main/assets/www/js/MyAdds.js
+++ platforms/android/app/src/main/assets/www/js/MyAdds.js
@@ -1,34 +1,32 @@
 var data;
+var claimedData;
 
-$("#add").click(function()
+function getDataById(id, dataModel)
 {
-     $('#myAddList').empty();
+  console.log('ID as param: ' + id);
+  var __found = jQuery.grep(dataModel, function(n, i) {return id == n.ID;})
+  console.log(__found);
 
-    var allAdds = new Object();
-    
-    var first = new Object();
-    first.ID = 1;
-    first.Location = "Luton";
-    first.Title = "some lovely title";
-    first.Description = "nice and long description";
-    first.DueDate = "2020-01-25";
+  return __found[0];
+}
 
-    var second = new Object();
-    second.ID = 2;
-    second.Location = "London";
-    second.Title = "some lovely title";
-    second.Description = "nice and long description";
-    second.DueDate = "2020-01-23";
+$(document).ready(function()
+{
+    getData();
+    initializeMyAdds();
+    initializeSelection();
+})
 
-    allAdds = [first, second];
-    var myString = JSON.stringify(allAdds);
-    data = jQuery.parseJSON(myString);
-   
+
+function initializeMyAdds () 
+{
+  $('#myAddList').empty();
+
     //console.log(myString);
     $.each(data, function(key,value) {
        
         $('#myAddList').append(
-            '<div data-id="'+value.ID+'" data-title="'+value.Title+'" data-location="'+value.Location+'">' +
+            '<div data-id="'+value.ID+'">' +
             '<a href="#" class="list-group-item list-group-item-action flex-column align-items-start">' +
                 '<div class="d-flex w-100 justify-content-between">' +
                   '<h5 class="mb-1 title">'+value.Title+ '</h5>' +
@@ -39,40 +37,89 @@ $("#add").click(function()
               '</a></div>'
             );
       }); 
-      makeSelection();
-     //$('#myAddList').append(' <button type="button" class="list-group-item list-group-item-action">Test stuff</button>')
-    
-    
-})
 
-function getDataById(id)
-{
-  console.log('ID as param: ' + id);
-  var __found = jQuery.grep(data, function(n, i) {return id == n.ID;})
-  console.log(__found);
+      $('#myClaimList').empty();
 
-  return __found[0];
+    //console.log(myString);
+    $.each(claimedData, function(key,value) {
+       
+        $('#myClaimList').append(
+            '<div data-id="'+value.ID+'">' +
+            '<a href="#" class="list-group-item list-group-item-action flex-column align-items-start">' +
+                '<div class="d-flex w-100 justify-content-between">' +
+                  '<h5 class="mb-1 title">'+value.Title+ '</h5>' +
+                  '<small>'+ value.DueDate +'</small>' +
+                '</div>' +
+                '<p class="mb-1">'+ value.Description +'</p>' +
+                '<small>'+ value.Location +'</small>' +
+              '</a></div>'
+            );
+      }); 
 }
 
-$(document).ready(function()
+function initializeSelection() 
 {
-    makeSelection();
-})
+  $("#myAddList div").click(function(e) {
 
-function makeSelection() 
+    $(".list-group .list-group-item").removeClass("active");
+    $(e.target).addClass("active");
+
+    //console.log($(this).attr('data-id'));
+    //console.log($(this).attr('data-title'));
+    //console.log($(this).attr('data-location'));
+     var selectedItem = (getDataById($(this).attr('data-id'), data));
+    
+      //var element = $('.mb-1 title');
+      //console.log("date "+element.text());
+     
+  });
+
+  $("#myClaimList div").click(function(e) {
+
+    $(".list-group .list-group-item").removeClass("active");
+    $(e.target).addClass("active");
+
+    //console.log($(this).attr('data-id'));
+    //console.log($(this).attr('data-title'));
+    //console.log($(this).attr('data-location'));
+     var selectedItem = (getDataById($(this).attr('data-id'), claimedData));
+    
+      //var element = $('.mb-1 title');
+      //console.log("date "+element.text());
+     
+  });
+}
+
+function getData() 
 {
-    $(".list-group div").click(function(e) {
+  var allAdds = new Object();
+    
+  var first = new Object();
+  first.ID = 1;
+  first.Location = "Luton";
+  first.Title = "some lovely title";
+  first.Description = "nice and long description";
+  first.DueDate = "2020-01-25";
 
-      $(".list-group .list-group-item").removeClass("active");
-      $(e.target).addClass("active");
+  var second = new Object();
+  second.ID = 2;
+  second.Location = "London";
+  second.Title = "some lovely title";
+  second.Description = "nice and long description";
+  second.DueDate = "2020-01-23";
 
-      //console.log($(this).attr('data-id'));
-      //console.log($(this).attr('data-title'));
-      //console.log($(this).attr('data-location'));
-       var selectedItem = (getDataById($(this).attr('data-id')));
-      
-        //var element = $('.mb-1 title');
-        //console.log("date "+element.text());
-       
-});
+  var third = new Object();
+  third.ID = 3;
+  third.Location = "Manchester";
+  third.Title = "some lovely title claimed";
+  third.Description = "nice and long description";
+  third.DueDate = "2020-01-23";
+
+  allAdds = [first, second];
+  var myString = JSON.stringify(allAdds);
+  data = jQuery.parseJSON(myString);
+
+  var allClaimed = [third];
+  var myString2 = JSON.stringify(allClaimed);
+  claimedData = jQuery.parseJSON(myString2);
 }
