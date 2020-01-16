047497a4c3bd1cce090387e52b513a8d876eaa5a
kvarcas91 <e.slutas@gmail.com>
2020-01-16 01:10:57 +0000

    register

--- www/js/register.js
+++ www/js/register.js
@@ -25,3 +25,20 @@ $(document).ready(function()
    })
 });
 
+(function() {
+    'use strict';
+    window.addEventListener('load', function() {
+      // Fetch all the forms we want to apply custom Bootstrap validation styles to
+      var forms = document.getElementsByClassName('needs-validation');
+      // Loop over them and prevent submission
+      var validation = Array.prototype.filter.call(forms, function(form) {
+        form.addEventListener('registerbtn', function(event) {
+          if (form.checkValidity() === false) {
+            event.preventDefault();
+            event.stopPropagation();
+          }
+          form.classList.add('was-validated');
+        }, false);
+      });
+    }, false);
+  })();
