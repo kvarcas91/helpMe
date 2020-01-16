44fd85a93129e4f1dbdc3a078eb8eb50e01ee767
kvarcas91 <e.slutas@gmail.com>
2020-01-16 02:25:50 +0000

    registration

--- www/js/register.js
+++ www/js/register.js
@@ -25,20 +25,159 @@ $(document).ready(function()
    })
 });
 
-(function() {
-    'use strict';
-    window.addEventListener('load', function() {
-      // Fetch all the forms we want to apply custom Bootstrap validation styles to
-      var forms = document.getElementsByClassName('needs-validation');
-      // Loop over them and prevent submission
-      var validation = Array.prototype.filter.call(forms, function(form) {
-        form.addEventListener('registerbtn', function(event) {
-          if (form.checkValidity() === false) {
-            event.preventDefault();
-            event.stopPropagation();
-          }
-          form.classList.add('was-validated');
-        }, false);
-      });
-    }, false);
-  })();
+$("#registerbtn").click(function()
+{
+    var firstnameErr;
+
+    // Validate FirstName
+    if (isEmpty($("#firstName")) == true) 
+    {
+        firstnameErr = "Enter first name";
+    }
+    else 
+    {
+        firstnameErr = "";
+    }
+    $("#firstNameValidation").text(firstnameErr);
+
+    // Validate LastName
+    if (isEmpty($("#lastName")) == true) 
+    {
+        firstnameErr = "Enter last name";
+    }
+    else 
+    {
+        firstnameErr = "";
+    }
+    $("#lastNameValidation").text(firstnameErr);
+
+    // Validate email
+    if (isEmailCorrect($("#email")) == false) 
+    {
+        firstnameErr = "Email is not valid";
+    }
+    else if (isEmailUnique($("#email")) == false) 
+    {
+        firstnameErr = "Email already exists";
+    }
+    else 
+    {
+        firstnameErr = "";
+    }
+    $("#emailValidation").text(firstnameErr);
+
+    // Validate username
+    if (isEmpty($("#username")) == true) 
+    {
+        firstnameErr = "Enter username";
+    }
+    else if (isUsernameUnique($("#username")) == false)
+    {
+        firstnameErr = "Username already exists";
+    }
+    else 
+    {
+        firstnameErr = "";
+    }
+    $("#usernameValidation").text(firstnameErr);
+
+    // Validate password
+    if (isEmpty($("#password")) == true) 
+    {
+        firstnameErr = "Enter password";
+    }
+    else 
+    {
+        firstnameErr = "";
+    }
+    $("#passwordValidation").text(firstnameErr);
+
+    // Validate Vpassword
+    if (isEmpty($("#vpassword")) == true) 
+    {
+        firstnameErr = "Enter password";
+    }
+    else 
+    {
+        firstnameErr = "";
+    }
+    $("#vpasswordValidation").text(firstnameErr);
+
+
+    // Validate password match
+    if (match($("#password"), $("#vpassword")) == false) 
+    {
+        firstnameErr = "Passwords doesn't match";
+    }
+    else 
+    {
+        firstnameErr = "";
+    }
+    $("#passwordValidation").text(firstnameErr);
+    $("#vpasswordValidation").text(firstnameErr);
+
+})
+
+function isEmpty (param) 
+{
+    var isEmpty = false;
+    if ($.trim(param.val()) == "") 
+    {
+        $(param).css({"border-color": "red"});
+        //param.val("");
+        isEmpty = true;
+    }
+    else 
+    {
+        $(param).css({"border-color": "#DCDCDC"});
+    }
+    console.log("IsEmpty (param - " + param.val() + "): " + isEmpty + ";");
+    return isEmpty;
+}
+
+function isEmailCorrect (param) 
+{
+    var email = (param).val().split("@");
+    console.log(param.val());
+    if (email[0].trim() != "" && email[1] == "study.beds.ac.uk") 
+    {
+        $(param).css({"border-color": "#DCDCDC"});
+        return true;
+    } 
+    else 
+    {
+        $(param).css({"border-color": "red"});
+
+        return false;
+    }
+}
+
+function isEmailUnique (param)
+{
+    return true;
+}
+
+function isUsernameUnique (param)
+{
+    return true;
+}
+
+function match (p1, p2)
+{
+    if (isEmpty(p1) == false && isEmpty(p2) == false)
+    {
+        if (p1.val() == p2.val())
+        {
+            $(p1).css({"border-color": "#DCDCDC"});
+            $(p2).css({"border-color": "#DCDCDC"});
+            return true;
+        }
+        else 
+        {
+            $(p1).css({"border-color": "red"});
+            $(p2).css({"border-color": "red"});
+            return false;
+        }
+    }
+    return false;
+}
